0503580b2e0a2886fe30e2e99a3c3db070da49ac
Lawjay Lee <lawjaylee1@icloud.com>
2026-04-30 02:54:18 -0700

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,4 +3,15 @@ vim.pack.add({
 	"https://github.com/folke/snacks.nvim",
 })
 
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd("colorscheme catppuccin-mocha")
+
+require("snacks").setup({
+	bigfile = { enabled = true },
+	picker = { enabled = true },
+})
+
+
+vim.g.mapleader = " "
+vim.g.localleader = "\\"
+
+vim.keymap.set("n", "<c-p>", Snacks.picker.files)
