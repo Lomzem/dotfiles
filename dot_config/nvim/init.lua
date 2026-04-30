ccdb8f7b301521216d354021fe4438c1798334a8
Lawjay Lee <lawjaylee1@icloud.com>
2026-04-30 04:31:49 -0700

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -112,3 +112,16 @@ end)
 
 vim.pack.add({ "https://github.com/mikavilpas/yazi.nvim" })
 vim.keymap.set("n", "<leader>pv", require("yazi").yazi)
+
+vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
+require("oil").setup({
+	view_options = {
+		show_hidden = true,
+	},
+	keymaps = {
+		["<C-s>"] = { false, desc = "Oil Save" },
+		["<C-p>"] = function()
+			Snacks.picker.files()
+		end,
+	},
+})
