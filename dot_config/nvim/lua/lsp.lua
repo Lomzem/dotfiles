ddc92b090fabfe6a918b171eb7a0ddc38eac3bf6
Lawjay Lee <lawjaylee1@icloud.com>
2026-04-30 03:38:22 -0700

    Update .config/nvim/lua/lsp.lua

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -16,3 +16,17 @@ local installed_lsp = {
 for _, lsp in ipairs(installed_lsp) do
 	vim.lsp.enable(lsp)
 end
+
+vim.pack.add({
+    "https://github.com/stevearc/conform.nvim" -- formatter
+})
+require("conform").setup({
+    format_on_save = {
+            timeout_ms = 500,
+    lsp_format = "fallback",
+    async = true,
+    },
+    formatters_by_ft = {
+        lua = {"stylua"},
+    },
+})
