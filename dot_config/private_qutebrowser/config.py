67959b97f657ac7cff4c97f145acd61ba1a4f942
Lawjay Lee <lawjaylee1@icloud.com>
2026-05-11 23:17:43 -0700

    Add .config/qutebrowser/autoconfig.yml Add .config/qutebrowser/bookmarks/urls Add .config/qutebrowser/config.py Add .config/qutebrowser/.chezmoiignore Add .config/qutebrowser/quickmarks Add .config/qutebrowser/greasemonkey/.keep Add .config/qutebrowser/qsettings/QtProject.conf

---
+++ dot_config/private_qutebrowser/config.py
@@ -0,0 +1,72 @@
+# qute://help/configuring.html#configpy
+
+from dataclasses import dataclass
+
+
+# Misc
+c.zoom.default = "110%"
+c.colors.webpage.darkmode.enabled = True
+c.url.searchengines["DEFAULT"] = "https://unduck.link?q={}"
+c.url.default_page = "file:///home/lomzem/coding/startpage/dist/index.html"
+c.url.start_pages = ["file:///home/lomzem/coding/startpage/dist/index.html"]
+
+# Tabs
+c.tabs.position = "right"
+c.tabs.width = "7%"
+
+# Fonts
+c.fonts.default_family = "monospace"
+c.fonts.default_size = "13pt"
+c.fonts.web.size.minimum = 18
+
+# Binds: misc
+config.bind("<ctrl-,>", "config-source")
+config.bind("<alt-shift-d>", "config-cycle colors.webpage.darkmode.enabled true false")
+config.bind("<space>nd", "clear-messages")
+config.bind("gh", "cmd-set-text --space :help")
+
+# Binds: tabs
+config.bind("<alt-k>", "tab-prev")
+config.bind("<alt-j>", "tab-next")
+config.bind("<ctrl-tab>", "tab-next")
+config.bind("<ctrl-shift-tab>", "tab-prev")
+config.bind("x", "tab-close")
+config.bind("<ctrl-t>", "cmd-set-text --space :open -t")
+
+# Binds: history
+config.bind("<alt-h>", "back")
+config.bind("<alt-l>", "forward")
+
+# Binds: navigation
+config.bind("k", "scroll-px 0 -100")
+config.bind("j", "scroll-px 0 100")
+
+config.bind("u", "scroll-px 0 -1500")
+config.bind("d", "scroll-px 0 1500")
+
+# Colors
+
+
+@dataclass
+class Theme:
+    bg: str
+    fg: str
+    card: str
+
+
+# https://github.com/vague-theme/vague
+THEME = Theme(
+    bg="#141415",
+    fg="#cdcdcd",
+    card="#333738",
+)
+
+c.colors.tabs.bar.bg = THEME.bg
+c.colors.tabs.even.bg = THEME.bg
+c.colors.tabs.odd.bg = THEME.bg
+c.colors.tabs.selected.even.bg = THEME.card
+c.colors.tabs.selected.odd.bg = THEME.card
+
+c.colors.statusbar.url.fg = THEME.fg
+c.colors.statusbar.url.success.http.fg = THEME.fg
+c.colors.statusbar.url.success.https.fg = THEME.fg
